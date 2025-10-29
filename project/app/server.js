2f4f83bf865087b2ebb50f56f532fc6f5db93f97
feewsworkspace-admin <nerara252@gmail.com>
2025-10-29 22:03:20 +0200

    docker

---
+++ project/app/server.js
@@ -0,0 +1,4 @@
+import express from "express";
+const app = express();
+app.get("/", (req, res) => res.send("Hello from Node + Docker!"));
+app.listen(3000, () => console.log("Server running on port 3000"));
